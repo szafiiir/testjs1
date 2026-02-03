f2b75b1c8ed121785fcc5c0d60e22fb9e6cab827
szafiiir <maniek2002388@gmail.com>
2026-02-03 21:56:59 +0100

    create functions getComputerChoice and getHumanChoice

--- main.js
+++ main.js
@@ -14,23 +14,23 @@ function getComputerChoice() {
   return choiceStr;
 }
 
-function getHumanChoice() {
-  const promptButton = document.querySelector("#promptButton");
-  const log = document.querySelector("#log");
+const promptButton = document.querySelector("#promptButton");
+const log = document.querySelector("#log");
 
-  promptButton.addEventListener("click", () => {
-    let prmpt = prompt("rock, paper or scissors?");
-    
-    if (prmpt === null) {
-      log.innerText = "OK, maybe next time.";
-    } else if (prmpt.toLowerCase() === "") {
-      log.innerText = "Try entering your choice again.";
-    } else if (prmpt.toLowerCase() === "rock") {
-      log.innerText = "You've chosen rock.";
-    } else if (prmpt.toLowerCase() === "paper") {
-      log.innerText = "You've chosen paper.";
-    } else if (prmpt.toLowerCase() === "scissors") {
-      log.innerText = "You've chosen scissors.";
-    }
-  });
+promptButton.addEventListener("click", getHumanChoice);
+
+function getHumanChoice() {
+  let prmpt = prompt("rock, paper or scissors?");
+
+  if (prmpt === null) {
+    log.innerText = "OK, maybe next time.";
+  } else if (prmpt.toLowerCase() === "") {
+    log.innerText = "Try entering your choice again.";
+  } else if (prmpt.toLowerCase() === "rock") {
+    log.innerText = "You've chosen rock.";
+  } else if (prmpt.toLowerCase() === "paper") {
+    log.innerText = "You've chosen paper.";
+  } else if (prmpt.toLowerCase() === "scissors") {
+    log.innerText = "You've chosen scissors.";
+  }
 }
